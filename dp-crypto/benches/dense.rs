cf0e390c8e5164cf32f1499a89aca91813abce6c
nicolas gailly <nikkolasg@gmail.com>
2025-11-28 15:53:58 +0100

    wip

--- dp-crypto/benches/dense.rs
+++ dp-crypto/benches/dense.rs
@@ -1,7 +1,7 @@
 use ark_bn254::Fr;
 use ark_poly::{Polynomial, evaluations::multivariate::DenseMultilinearExtension};
 use divan::Bencher;
-use dp_crypto::poly::{dense::DensePolynomial, slice::SmartSlice};
+use dp_crypto::poly::{dense::{DensePolynomial, FixOrder}, slice::SmartSlice};
 
 fn main() {
     // Run registered benchmarks.
@@ -23,6 +23,33 @@ fn arkyper_dense_eval_static(b: Bencher, n: usize) {
             DensePolynomial::new_from_smart_slice(SmartSlice::Borrowed(s)).evaluate(&r)
         })
 }
+
+#[divan::bench(args = LENS)]
+fn arkyper_dense_low_to_high_parallel(b: Bencher, n: usize) {
+    b.with_inputs(|| arkworks_static_evals(2u32.pow(n as u32) as usize))
+    .bench_local_values(|s| {
+        let r_len = s.len().ilog2()/2;
+        let r = (0..r_len).map(|i| Fr::from(i as u64)).collect::<Vec<_>>();
+        let mut poly = DensePolynomial::new_from_smart_slice(SmartSlice::Owned(s));
+        for r_i in r {
+            poly.par_fix_mut(r_i, FixOrder::LowToHigh);
+        }
+    })
+}
+
+#[divan::bench(args = LENS)]
+fn arkyper_dense_high_to_low_parallel(b: Bencher, n: usize) {
+    b.with_inputs(|| arkworks_static_evals(2u32.pow(n as u32) as usize))
+    .bench_local_values(|s| {
+        let r_len = s.len().ilog2()/2;
+        let r = (0..r_len).map(|i| Fr::from(i as u64)).collect::<Vec<_>>();
+        let mut poly = DensePolynomial::new_from_smart_slice(SmartSlice::Owned(s));
+        for r_i in r {
+            poly.par_fix_mut(r_i, FixOrder::HighToLow);
+        }
+    })
+}
+
 
 #[divan::bench(args = LENS)]
 fn arkworks_dense_eval_static(b: Bencher, n: usize) {
